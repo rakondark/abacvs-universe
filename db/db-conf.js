e99d341ab604d7145925aa45c6ecb2ec2d1120a5
rakondark <rakondark@gmail.com>
2020-03-25 02:21:52 +0100

    Update db-conf.js

--- db/db-conf.js
+++ db/db-conf.js
@@ -7,6 +7,9 @@ const user = process.env.DB_USER || 'praetorians';
 // Get the Password for DB from Environment or use default
 <<<<<<< HEAD
 const password = process.env.DB_PASS || 'xxxxxxxxxxx';
+=======
+const password = process.env.DB_PASS || 'xxxxxxxxx';
+>>>>>>> 4807c2d537252c3ccc8dc1a367a7803d4157751b
 
 // Get the Database from Environment or use default
 const database = process.env.DB_DATABASE || 'praetorians';
